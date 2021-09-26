5ba665fca8188af0229ca31566988084deccb598
cinexin <migueguillamon@gmail.com>
2021-09-26 12:48:30 +0200

    Apply themes posts in the feed page

--- angular/src/app/post/post.component.ts
+++ angular/src/app/post/post.component.ts
@@ -8,10 +8,23 @@ import {Component, Input, OnInit} from '@angular/core';
 export class PostComponent implements OnInit {
 
   @Input() post: any;
+  fakeId: string = 'fakeid';
 
   constructor() { }
 
   ngOnInit(): void {
+    const removeLeadingNumbers = (s: string) => {
+      const isNumber = (n: any) => {
+        n = Number(n);
+        return isNaN(n) ? false : true;
+      }
+      if (s && isNumber(s[0])) {
+        s = removeLeadingNumbers(s.substr(1))
+      }
+      return s;
+    }
+
+    this.fakeId = removeLeadingNumbers(this.post._id);
   }
 
 }
