19040290659b5f51a2946988007ec3a2b86c37e1
cinexin <migueguillamon@gmail.com>
2021-10-09 13:39:48 +0200

    - Profile: part II

--- angular/src/app/page-profile/page-profile.component.ts
+++ angular/src/app/page-profile/page-profile.component.ts
@@ -1,6 +1,9 @@
 import {Component, Inject, OnInit} from '@angular/core';
 import {Title} from "@angular/platform-browser";
 import {DOCUMENT} from "@angular/common";
+import {UserDataService} from "../user-data.service";
+import {ApiService} from "../api.service";
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
   selector: 'app-page-profile',
@@ -11,6 +14,9 @@ export class PageProfileComponent implements OnInit {
 
   constructor(
     private title: Title,
+    private userDataService: UserDataService,
+    private apiService: ApiService,
+    private activatedRoute: ActivatedRoute,
     @Inject(DOCUMENT) private document: Document
   ) { }
 
@@ -20,6 +26,42 @@ export class PageProfileComponent implements OnInit {
     if (sidebar) {
       sidebar.classList.add('d-none');
     }
+    const paramId = this.activatedRoute.snapshot.params.userId;
+     this.userDataService.getUserData.subscribe((user: any) => {
+       if (user._id == paramId) {
+         console.log('Your profile');
+         this.setComponentValues(user);
+       } else {
+         console.log('Not your profile');
+       }
+     });
   }
 
+  public randomFriends: any[] = [];
+  public totalFriends: number = 0;
+  public posts: object[] = [];
+  public profilePicture: string = 'default-avatar';
+  public usersName: string = '';
+  public usersEmail: string = '';
+
+  public canAddUser: boolean = false;
+  public canSendMessage: boolean = true;
+  public postsToShow: number = 6;
+
+  showMorePosts(): void {
+    this.postsToShow += 6;
+  }
+
+  backToTop(): void {
+    this.document.body.scrollTop = this.document.documentElement.scrollTop = 0;
+  }
+
+  setComponentValues(user: any): void {
+    this.randomFriends = user.random_friends;
+    this.profilePicture = user.profile_image;
+    this.posts = user.posts;
+    this.usersName = user.name;
+    this.usersEmail = user.email;
+    this.totalFriends = user.friends.length;
+  }
 }
