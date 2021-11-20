e732187d42653f5cc2c74ed7b77ef4e23ee0e3e0
cineexin <migueguillamon@gmail.com>
2021-11-20 15:16:48 +0100

    - Cleaning up the routes

--- mvc/routes/users.js
+++ mvc/routes/users.js
@@ -5,24 +5,32 @@ const middleware = require('./middleware/middleware');
 const usersCtrl = require('../controllers/user');
 const fakeUsersCtrl = require('../controllers/fake-users');
 
-
+// Login in & Register
 router.post('/register', usersCtrl.registerUser);
 router.post('/login', usersCtrl.loginUser);
+
+// Basic GET requests
 router.get('/all', usersCtrl.getAllUsers);
 router.get('/generate-feed', middleware.authorize, usersCtrl.generateFeed);
-router.get('/get-search-results', middleware.authorize, usersCtrl.getSearchResults);
-router.get('/get-friend-requests', middleware.authorize, usersCtrl.getFriendRequests);
 router.get('/get-user-data/:userid', middleware.authorize, usersCtrl.getUserData);
+router.get('/get-search-results', middleware.authorize, usersCtrl.getSearchResults);
+
+// Routes Handling friend requests
+router.get('/get-friend-requests', middleware.authorize, usersCtrl.getFriendRequests);
 router.post('/make-friend-request/:from/:to', middleware.authorize, usersCtrl.makeFriendRequest);
 router.post('/resolve-friend-request/:from/:to', middleware.authorize, usersCtrl.resolveFriendRequest);
+
+// Routes handling posts
 router.post('/create-post', middleware.authorize, usersCtrl.createPost);
 router.post('/like-unlike/:ownerId/:postId', middleware.authorize, usersCtrl.likeUnlike);
 router.post('/post-comment/:ownerId/:postId', middleware.authorize, usersCtrl.commentOnPost);
+
+// Routes handling messages
 router.post('/send-message/:to', middleware.authorize, usersCtrl.sendMessage);
 router.post('/reset-message-notifications', middleware.authorize, usersCtrl.resetMessageNotifications);
 router.delete('/messages/:id', middleware.authorize, usersCtrl.deleteMessage);
 
-
+// Development & testing only!
 router.post('/create-fake-users', fakeUsersCtrl.createFakeUsers);
 
 module.exports = router;
